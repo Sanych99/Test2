b8a76582f94758047ebb44256b751309e60b7d07
alexandr.tsaregorodtsev <alexandr.tsaregorodtsev@jazzsoft.kz>
2015-02-27 20:47:34 +0600

    Optimization

--- ibot_os/ibot_events/src/ibot_events_sup.erl
+++ ibot_os/ibot_events/src/ibot_events_sup.erl
@@ -10,18 +10,24 @@
 
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CHILD_PARAMS(I, Type, Param), {I, {I, start_link, [Param]}, permanent, 5000, Type, [I]}).
 
 %% ===================================================================
 %% API functions
 %% ===================================================================
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+  %%{ok, Pid} = gen_event:start_link("my_event_bus"),
+  {ok, Pid} = gen_server:start_link({local, test1}, test, [], []),
+  io:format("~p~n", [Pid]),
+  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+    % Event Manager Child
+    EvenManager = ?CHILD_PARAMS(gen_event, worker, gen_event),
+    {ok, { {one_for_one, 5, 10}, [EvenManager]} }.
 
