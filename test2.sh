7f7feac78af2733fb66ad9a88009cfa21da61a85
Alexandr.Tsaregorodtsev <sanych99@gmail.com>
2015-06-14 23:20:10 +0600

    ch

---
+++ test2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+clear
+echo "Direcory release"
+cd ~/iBotOS/iBotOS/ibot_os/rel2/ibot_os/bin/
+echo "Run release"
+./ibot_os console -eval 'ibot_core_srv_project_info_loader:load_core_config().'
+
